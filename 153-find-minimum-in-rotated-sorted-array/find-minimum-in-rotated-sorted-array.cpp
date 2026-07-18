00ba0385a963fe20b4d72acd84bc4e2aec0322f0
AyushiKarwa <140518787+AyushiKarwa@users.noreply.github.com>
2026-07-18 19:03:45 +0530

    Time: 0 ms (100.00%) | Memory: 14.3 MB (11.89%) - LeetSync

--- 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.cpp
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.cpp
@@ -8,7 +8,7 @@ public:
         int low=0,high=n-1;
         int res=-1;
         while(low<=high){
-            int mid=(low+high)/2;
+            int mid=low+(high-low)/2;
             if(nums[mid]>nums[n-1]){
                 low=mid+1;
             }
