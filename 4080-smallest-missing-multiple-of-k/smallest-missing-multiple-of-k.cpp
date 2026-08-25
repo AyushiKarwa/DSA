a34376d363ea3cf36279d292d146c8b1fefd55be
AyushiKarwa <140518787+AyushiKarwa@users.noreply.github.com>
2026-08-25 17:48:33 +0530

    Time: 0 ms (100.00%) | Memory: 24.2 MB (67.36%) - LeetSync

--- 4080-smallest-missing-multiple-of-k/smallest-missing-multiple-of-k.cpp
+++ 4080-smallest-missing-multiple-of-k/smallest-missing-multiple-of-k.cpp
@@ -1,14 +1,13 @@
 class Solution {
 public:
     int missingMultiple(vector<int>& nums, int k) {
-        uint64_t x[2] = {0, 0};
-        for (auto& n : nums)
-            if (n % k == 0) {
-                int i = n / k - 1;
-                x[i >> 6] |= 1ULL << (i & 63);
+        int i=1;
+        while(true){
+            if(find(nums.begin(),nums.end(),k*i)==nums.end()){
+                return k*i;
             }
-
-        int z = x[0] == -1ULL;
-        return (z * 64 + bit_width(++x[z] & -x[z])) * k;
+            i++;
+        }
+        return 0;
     }
 };
