29316429ee9512815024e1789a40f13bd099783d
AyushiKarwa <140518787+AyushiKarwa@users.noreply.github.com>
2026-09-16 10:50:38 +0530

    Time: 2 ms (79.82%) | Memory: 16 MB (28.90%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.cpp
+++ 22-generate-parentheses/generate-parentheses.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
-    void solve(string current,int open,int close,int n,vector<string> &ans){
-        if(current.length()==2*n){
-            ans.push_back(current);
+    vector<string> ans;
+    void solve(string s,int open,int close,int n){
+        if(s.size()==2*n){
+            ans.push_back(s);
             return;
         }
         if(open<n){
-           solve(current+'(',open+1,close,n,ans);
+            solve(s+'(',open+1,close,n);
         }
         if(close<open){
-           solve(current+')',open,close+1,n,ans);
+            solve(s+')',open,close+1,n);
         }
-
     }
     vector<string> generateParenthesis(int n) {
-        vector<string> ans;
-        solve("",0,0,n,ans);
+        
+        solve("",0,0,n);
         return ans;
     }
 };
