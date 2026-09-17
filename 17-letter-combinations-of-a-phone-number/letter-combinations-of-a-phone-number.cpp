97cd5897c4383683ebda0044d0936034c9e692da
AyushiKarwa <140518787+AyushiKarwa@users.noreply.github.com>
2026-09-17 13:47:40 +0530

    Time: 0 ms (100.00%) | Memory: 9.6 MB (12.14%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
@@ -3,25 +3,26 @@ public:
 vector<string> mp={
     "","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"
 };
-    void solve(string &digits,int idx,string curr,vector<string> &ans){
-        if(idx==digits.size()){
+vector<string> ans;
+    void solve(string &digits,int i,string curr){
+        if(i==digits.size()){
             ans.push_back(curr);
             return;
         }
-        string letters=mp[digits[idx]-'0'];
+        string letters=mp[digits[i]-'0'];
         for(char ch:letters){
             curr.push_back(ch);
-            solve(digits,idx+1,curr,ans);
+            solve(digits,i+1,curr);
             curr.pop_back();
         }
 
     }
     vector<string> letterCombinations(string digits) {
-        vector<string> ans;
+       
         if(digits.empty()){
             return ans;
         }
-        solve(digits,0,"",ans);
+        solve(digits,0,"");
         return ans;
     }
 };
